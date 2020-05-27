b96b7efc0551273fb395ba88cb9401302367c16a
Mad Ting <samtastic1337@gmail.com>
2020-05-27 17:57:23 +0200

    Basic thinsg are working

--- routes/signup.js
+++ routes/signup.js
@@ -1,33 +1,48 @@
-const express = require('express')
-const router = express.Router()
-const client = require('../database/database')
+const express = require("express");
+const router = express.Router();
+const client = require("../database/database");
 
-
-
-const createPatient  = (req, res) => {
-
- const{insurance_name, insurance_number,email ,password,first_name,last_name, mobile_number  }   = req.body
- client
- .query("INSERT INTO patient_profile (insurance_name, insurance_number, email , password, first_name, last_name, mobile_number) VALUES($1, $2, $3, $4; $5, $6, $7 );", [insurance_name, insurance_number,email ,password,first_name,last_name, mobile_number])
- .then ((data) => res.json(data))
- .catch((e) => res.sendStatus(404).json(e))
-
+const createPatient = (req, res) => {
+  const {
+    insurance_company,
+    insurance_number,
+    email,
+    password,
+    first_name,
+    last_name,
+    dob,
+    mobile_number,
+  } = req.body;
+  client
+    .query(
+      "INSERT INTO patient_profile (insurance_company, insurance_number, email , password, first_name, dob, last_name, mobile_number) VALUES($1, $2, $3, $4, $5, $6, $7, $8)",
+      [
+        insurance_company,
+        insurance_number,
+        email,
+        password,
+        first_name,
+        last_name,
+        dob,
+        mobile_number,
+      ]
+    )
+    .then((data) => res.json(data))
+    .catch((e) => res.sendStatus(404).json(e));
 };
 
-const createDcoctor  = (req, res) => {
+const createDcoctor = (req, res) => {
+  const { email, password, website, contact_number } = req.body;
+  client
+    .query(
+      "INSERT INTO doctor_profile ( email , password,  website, contact_number) VALUES($1, $2, $3, $4, );",
+      [email, password, website, contact_number]
+    )
+    .then((data) => res.json(data))
+    .catch((e) => res.sendStatus(404).json(e));
+};
 
-    const{email , password, website, contact_number  }  = req.body;
-    client
-    .query("INSERT INTO patient_profile ( email , password,  website, contact_number) VALUES($1, $2, $3, $4; );", [email ,password, website, contact_number])
-    .then ((data) => res.json(data))
-    .catch((e) => res.sendStatus(404).json(e))
-   
-   };
+router.post("/create/patient", createPatient);
+router.post("/create/doctor", createDcoctor);
 
-
-router.post('/create/patient', createPatient);
-router.post('create/doctor', createDcoctor);
-
-module.exports = router
-
-
+module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,28 @@
+require("dotenv").config();
 
 // Basic Node and Express Server
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 // Database Endpoint Export
-const test = require('./routes/login')
-const test2 = require('./routes/signup')
+// const logintest = require('./routes/login')
+const signuptest = require("./routes/signup");
 // Loading bodyParser Module e.g. app.use(express.urlencoded({extended: true}))
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 // Port Server is listening to
-const port = 2000
+const port = 2000;
 
 // BodyParser for pasing data in Json or from req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // express Router usage of Database
-app.use('/test', test);
+// app.use('/test', logintest);
 // Router Database creation
-app.use('/', test2)
+app.use("/signup", signuptest);
 // basic endpoint with json response
-app.get('/', (req, res) => {
-res.status(200).json({success : true})})
-
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true });
+});
 
 // Listening to Server e.g. Starting Server
-app.listen(
-    port, () => console.log(`Server listening on port ${port}`)
-);
+app.listen(port, () => console.log(`Server listening on port ${port}`));
